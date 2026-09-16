48e4e1477224b58d6be0159f87d64931baa5863f
Aryan Pratap Singh <aryansingh87615@gmail.com>
2026-09-16 23:18:58 +0530

    edited utils.py file

--- src/student_dashboard/utils.py
+++ src/student_dashboard/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from student_dashboard.exception import custom_error
 import pickle
 
@@ -24,15 +25,32 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            parameters = param[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model, parameters, cv=3)
+            gs.fit(X_train, y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+            
             
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
+            
             report[list(models.keys())[i]] = test_model_score
             
         return report
+    
+    except Exception as e:
+        raise custom_error(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise custom_error(e, sys)
